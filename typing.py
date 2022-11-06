dd23147bbec4029110308a628a6476b0d107a75a
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2022-11-06 14:45:45 +0900

    up

--- typing.py
+++ typing.py
@@ -1,12 +1,15 @@
-from typing import Callable
-
-
 class __Dummy:
     pass
     def __getitem__(self, key):
         pass
+
+def cast(type, value):
+    return value
+
+
 __dummy = __Dummy()
 
 Optional = __dummy
 Union = __dummy
 Any = __dummy
+TypedDict = dict
